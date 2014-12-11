a48e2a466c98f1f159a6a5fd1b7ded5f967e14f1
Prahlad Nrsimha Das <pnd@mayapurmedia.com>
2014-12-11 14:38:45 +0530

    Fixed author info.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     name='jingo-markdown',
     version="1.3",
     description='Jingo (Jinja2) extension and filter for parse markdown text markup.',
-    author='Prahlad Nrsimha Das - Petr Vacha',
+    author='Prahlad Nrsimha Das (Petr Vacha) - Mayapur Media',
     author_email='pnd@mayapurmedia.com',
     url='https://github.com/mayapurmedia/jingo-markdown',
     install_requires=['Markdown'],
